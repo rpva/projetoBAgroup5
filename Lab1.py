96c639199d595e5b8737780aff70c182184319ed
AntonioAlem <Molengo1>
2020-02-10 15:22:21 +0000

    col (prints a collumn)

--- Lab1.py
+++ Lab1.py
@@ -7,12 +7,13 @@ data = pd.read_csv('covtype.csv', index_col='Cover_Type(Class)', sep=';')  # imp
 print(data.head()) # outputs table contents into console
 
 print(data.shape)
+col = data['Elevation']
+print(col)
 
-
-fig = plt.figure(figsize=(10, 7))
-mv = {}
-for var in data:
-    mv[var] = data[var].isna().sum()
-    bar_chart(plt.gca(), mv.keys(), mv.values(), 'Number of missing values per variable', var, 'nr. missing values')
-fig.tight_layout()
-plt.show()
+# fig = plt.figure(figsize=(10, 7))
+# mv = {}
+# for var in data:
+#     mv[var] = data[var].isna().sum()
+#     bar_chart(plt.gca(), mv.keys(), mv.values(), 'Number of missing values per variable', var, 'nr. missing values')
+# fig.tight_layout()
+# plt.show()
