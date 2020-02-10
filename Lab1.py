ad966c2c035192deb153cde5fe239908475c4663
suscardoso <susanacostacardoso88@gmail.com>
2020-02-10 16:14:19 +0000

    notas aula

--- Lab1.py
+++ Lab1.py
@@ -9,10 +9,10 @@ print(data.head()) # outputs table contents into console
 print(data.shape)
 
 
-fig = plt.figure(figsize=(10, 7))
-mv = {}
-for var in data:
-    mv[var] = data[var].isna().sum()
-    bar_chart(plt.gca(), mv.keys(), mv.values(), 'Number of missing values per variable', var, 'nr. missing values')
-fig.tight_layout()
-plt.show()
+# fig = plt.figure(figsize=(10, 7))
+# mv = {}
+# for var in data:
+#     mv[var] = data[var].isna().sum()
+#     bar_chart(plt.gca(), mv.keys(), mv.values(), 'Number of missing values per variable', var, 'nr. missing values')
+# fig.tight_layout()
+# plt.show()
