5e80fef34d0714b7ed2afb77229888b001510319
suscardoso <susanacostacardoso88@gmail.com>
2020-02-11 13:47:32 +0000

    Ficheiro com a minha parte. Pontos 23, 24 e 25. Os gráficos ainda não estão bonitinhos, têm de ser melhorados.

--- Lab1.py
+++ Lab1.py
@@ -8,7 +8,8 @@ import matplotlib.pyplot as plt
 # data = pd.read_csv('covtype.csv', index_col='Cover_Type(Class)', sep=';')  # import the dataset into the dataframe, using pandas
 
 data = pd.read_csv('covtype.csv', sep=';')
-dataSample = data.sample(frac=0.1) # we use a sample with 10% of the data
+# we use a sample with 10% of the data
+dataSample = data.sample(frac=0.1)
 
 print(data.head()) # prints/outputs table contents into console. By default, only the first 5 records
 print(data.head(7)) # prints/outputs table contents into console. By default, only the first 5 records
@@ -38,13 +39,56 @@ print(data.dtypes) # give us the type of the variables
 
 # Variables distribution
 
-# boxplots for each variable on the same graph
+# (22) boxplots for each variable on the same graph
 
 dataSample.boxplot(figsize=(10, 6))
 plt.show()
 
-# singular boxplot for each variable
+# (23) singular boxplot for each variable
 
+columns = dataSample.select_dtypes(include='number').columns
+rows, cols = choose_grid(len(columns))
+plt.figure()
+
+fig, axs = plt.subplots(rows, cols, figsize=(cols*4, rows*4), squeeze=False)
+i, j = 0, 0
+for n in range(len(columns)):
+    axs[i, j].set_title('Boxplot for %s'%columns[n])
+    axs[i, j].boxplot(data[columns[n]].dropna().values)
+    i, j = (i + 1, 0) if (n+1) % cols == 0 else (i, j + 1)
+fig.tight_layout()
+plt.show()
+
+# (24) histogram for each numeric variable
+
+columns = dataSample.select_dtypes(include='number').columns
+rows, cols = choose_grid(len(columns))
+plt.figure()
+fig, axs = plt.subplots(rows, cols, figsize=(cols*4, rows*4), squeeze=False)
+i, j = 0, 0
+for n in range(len(columns)):
+    axs[i, j].set_title('Histogram for %s'%columns[n])
+    axs[i, j].set_xlabel(columns[n])
+    axs[i, j].set_ylabel("probability")
+    axs[i, j].hist(data[columns[n]].dropna().values, 'auto')
+    i, j = (i + 1, 0) if (n+1) % cols == 0 else (i, j + 1)
+fig.tight_layout()
+plt.show()
+
+# (25) histograms for categorical variables - bar charts (counting the frequency of each value for each variable)
+# it doesn't give anything because we don't have categorical variables, they are all numerical
+
+columns = dataSample.select_dtypes(include='category').columns
+rows, cols = choose_grid(len(columns))
+plt.figure()
+fig, axs = plt.subplots(rows, cols, figsize=(cols*4, rows*4), squeeze=False)
+i, j = 0, 0
+for n in range(len(columns)):
+    counts = data[columns[n]].dropna().value_counts(normalize=True)
+    bar_chart(axs[i, j], counts.index, counts.values, 'Histogram for %s'%columns[n], columns[n], 'probability')
+    i, j = (i + 1, 0) if (n+1) % cols == 0 else (i, j + 1)
+fig.tight_layout()
+plt.show()
 
 # Granularity
 
