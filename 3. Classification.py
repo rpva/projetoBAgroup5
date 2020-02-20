5e99e84c4c5b05657649ded1328ae00d23f5c80f
AntonioAlem <Molengo1>
2020-02-20 16:36:32 +0000

    3. Classifications

--- 3. Classification.py
+++ 3. Classification.py
@@ -8,6 +8,10 @@ from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 import ipynb.fs.defs.functions as func
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import export_graphviz
+from sklearn.ensemble import RandomForestClassifier
+
+
 
 # Training Strategy: hold-out (train_test_split): used in the presence of large thousands of records;
 
@@ -71,27 +75,66 @@ trnX, tstX, trnY, tstY = train_test_split(X, y, train_size=0.7, stratify=y)
 # func.multiple_line_chart(plt.gca(), nvalues, values, 'KNN variants', 'n', 'accuracy', percentage=True)
 # plt.show()
 
-# Decision Trees
+# # Decision Trees
+#
+# min_samples_leaf = [.05, .025, .01, .0075, .005, .0025, .001]
+# max_depths = [5, 10, 25, 50]
+# criteria = ['entropy', 'gini']
+#
+# plt.figure()
+# fig, axs = plt.subplots(1, 2, figsize=(16, 4), squeeze=False)
+# for k in range(len(criteria)):
+#     f = criteria[k]
+#     values = {}
+#     for d in max_depths:
+#         yvalues = []
+#         for n in min_samples_leaf:
+#             tree = DecisionTreeClassifier(min_samples_leaf=n, max_depth=d, criterion=f)
+#             tree.fit(trnX, trnY)
+#             prdY = tree.predict(tstX)
+#             yvalues.append(metrics.accuracy_score(tstY, prdY))
+#             # print(metrics.accuracy_score(tstY, prdY))
+#         values[d] = yvalues
+#         func.multiple_line_chart(axs[0, k], min_samples_leaf, values, 'Decision Trees with %s criteria' % f,
+#                                  'nr estimators'
+#                                  , 'accuracy', percentage=True)
+# plt.show()
+#
+# tree = DecisionTreeClassifier(max_depth=3)
+# tree.fit(trnX, trnY)
+#
+# dot_data = export_graphviz(tree, out_file='dtree.dot', filled=True, rounded=True, special_characters=True)
+# # Convert to png
+#
+# from subprocess import call
+#
+# call(['dot', '-Tpng', 'dtree.dot', '-o', 'dtree.png', '-Gdpi=600'])
+#
+# plt.figure(figsize=(14, 18))
+# plt.imshow(plt.imread('dtree.png'))
+# plt.axis('off')
+# plt.show()
 
-min_samples_leaf = [.05, .025, .01, .0075, .005, .0025, .001]
+# Random Trees
+
+n_estimators = [5, 10, 25, 50, 75, 100, 150, 200, 250, 300]
 max_depths = [5, 10, 25, 50]
-criteria = ['entropy', 'gini']
+max_features = ['sqrt', 'log2']
 
 plt.figure()
-fig, axs = plt.subplots(1, 2, figsize=(16, 4), squeeze=False)
-for k in range(len(criteria)):
-    f = criteria[k]
+fig, axs = plt.subplots(1, 2, figsize=(10, 4), squeeze=False)
+for k in range(len(max_features)):
+    f = max_features[k]
     values = {}
     for d in max_depths:
         yvalues = []
-        for n in min_samples_leaf:
-            tree = DecisionTreeClassifier(min_samples_leaf=n, max_depth=d, criterion=f)
-            tree.fit(trnX, trnY)
-            prdY = tree.predict(tstX)
+        for n in n_estimators:
+            rf = RandomForestClassifier(n_estimators=n, max_depth=d, max_features=f)
+            rf.fit(trnX, trnY)
+            prdY = rf.predict(tstX)
             yvalues.append(metrics.accuracy_score(tstY, prdY))
-            # print(metrics.accuracy_score(tstY, prdY))
         values[d] = yvalues
-        func.multiple_line_chart(axs[0, k], min_samples_leaf, values, 'Decision Trees with %s criteria' % f,
-                                 'nr estimators'
-                                 , 'accuracy', percentage=True)
+    func.multiple_line_chart(axs[0, k], n_estimators, values, 'Random Forests with %s features' % f, 'nr estimators',
+                             'accuracy', percentage=True)
+
 plt.show()
