dc99aad6e99fdf5454cd8c97ece26b66b4da219c
AntonioAlem <Molengo1>
2020-02-25 20:31:50 +0000

    3. Classifications - acertos

--- 3. Classification.py
+++ 3. Classification.py
@@ -83,48 +83,48 @@ def model_performance(tstY, prdY, n, d=' ', k=' ', n_name='n', d_name=' ', k_nam
 # multiple_line_chart(plt.gca(), nvalues, values, 'KNN variants', 'Number of Neighbors (K)', 'Accuracy', percentage=True)
 # plt.show()
 #
-# # Decision Trees
-# min_samples_leaf = [.05, .025, .01, .0075, .005, .0025, .001]
-# max_depths = [5, 10, 25, 50]
-# criteria = ['entropy', 'gini']
+# Decision Trees
+min_samples_leaf = [.05, .025, .01, .0075, .005, .0025, .001]
+max_depths = [5, 10, 25, 50]
+criteria = ['entropy', 'gini']
+
+plt.figure()
+fig, axs = plt.subplots(1, 2, figsize=(16, 4), squeeze=False)
+for k in range(len(criteria)):
+    f = criteria[k]
+    values = {}
+    for d in max_depths:
+        yvalues = []
+        for n in min_samples_leaf:
+            tree = DecisionTreeClassifier(min_samples_leaf=n, max_depth=d, criterion=f)
+            tree.fit(trnX, trnY)
+            prdY = tree.predict(tstX)
+            yvalues.append(metrics.accuracy_score(tstY, prdY))
+            # print(metrics.accuracy_score(tstY, prdY))
+        values[d] = yvalues
+        multiple_line_chart(axs[0, k], min_samples_leaf, values, 'Decision Trees with %s criteria' % f,
+                            'Number of Estimators'
+                            , 'Accuracy', percentage=True)
+plt.show()
+
+# Following lines show the learned tree, using the graphviz package
+# NOT WORKING
+# tree = DecisionTreeClassifier(max_depth=3)
+# tree.fit(trnX, trnY)
 #
-# plt.figure()
-# fig, axs = plt.subplots(1, 2, figsize=(16, 4), squeeze=False)
-# for k in range(len(criteria)):
-#     f = criteria[k]
-#     values = {}
-#     for d in max_depths:
-#         yvalues = []
-#         for n in min_samples_leaf:
-#             tree = DecisionTreeClassifier(min_samples_leaf=n, max_depth=d, criterion=f)
-#             tree.fit(trnX, trnY)
-#             prdY = tree.predict(tstX)
-#             yvalues.append(metrics.accuracy_score(tstY, prdY))
-#             # print(metrics.accuracy_score(tstY, prdY))
-#         values[d] = yvalues
-#         multiple_line_chart(axs[0, k], min_samples_leaf, values, 'Decision Trees with %s criteria' % f,
-#                             'Number of Estimators'
-#                             , 'Accuracy', percentage=True)
+# dot_data = export_graphviz(tree, out_file='dtree.dot', filled=True, rounded=True, special_characters=True)
+# # Convert to png
+# call(['dot', '-Tpng', 'dtree.dot', '-o', 'dtree.png', '-Gdpi=600'])
+#
+# plt.figure(figsize=(14, 18))
+# plt.imshow(plt.imread('dtree.png'))
+# plt.axis('off')
 # plt.show()
-#
-# ## Following lines show the learned tree, using the graphviz package
-# ## NOT WORKING
-# # tree = DecisionTreeClassifier(max_depth=3)
-# # tree.fit(trnX, trnY)
-# #
-# # dot_data = export_graphviz(tree, out_file='dtree.dot', filled=True, rounded=True, special_characters=True)
-# # # Convert to png
-# # call(['dot', '-Tpng', 'dtree.dot', '-o', 'dtree.png', '-Gdpi=600'])
-# #
-# # plt.figure(figsize=(14, 18))
-# # plt.imshow(plt.imread('dtree.png'))
-# # plt.axis('off')
-# # plt.show()
-#
+
 # # Random Trees
 # n_estimators = [5, 10, 25, 50, 75, 100, 150, 200, 250, 300]
 # max_depths = [5, 10, 25, 50]
-# max_features = ['Square Root', 'logarithm base 2']
+# max_features = ['sqrt', 'log2']
 #
 # plt.figure()
 # fig, axs = plt.subplots(1, 2, figsize=(10, 4), squeeze=False)
@@ -142,46 +142,46 @@ def model_performance(tstY, prdY, n, d=' ', k=' ', n_name='n', d_name=' ', k_nam
 #     multiple_line_chart(axs[0, k], n_estimators, values, 'Random Forests with %s features' % f, 'Number of Estimators',
 #                         'Accuracy', percentage=True)
 # plt.show()
+
+
+# # Gradient Boosting
+# def GB(data1, save=None):
+#     from sklearn.ensemble import GradientBoostingClassifier
+#     print('Gradient Boosting Started \n')
+#       # Existing error in Cover_Type(Class)
+#     # y: np.ndarray = data1.pop('Cover_Type(Class)').values
+#     # X: np.ndarray = data1.values
+#     labels = pd.unique(y)
 #
-
-# Gradient Boosting
-def GB(data1, save=None):
-    from sklearn.ensemble import GradientBoostingClassifier
-    print('Gradient Boosting Started \n')
-      # Existing error in Cover_Type(Class)
-    # y: np.ndarray = data1.pop('Cover_Type(Class)').values
-    # X: np.ndarray = data1.values
-    labels = pd.unique(y)
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, stratify=y)
-
-    estimators = [5, 50, 100, 150, 200, 300]
-    l_rate = [0.1]
-    max_depth = [2, 10, 20]
-
-    yvalues = []
-    for l in range(len(l_rate)):
-        values = {}
-        for d in range(len(max_depth)):
-            yvalues = []
-            for e in range(len(estimators)):
-                gbc = GradientBoostingClassifier(n_estimators=estimators[e], learning_rate=l_rate[l],
-                                                 max_depth=max_depth[d])
-                gbc.fit(X_train, y_train)
-                gbc.score(X_test, y_test)
-                prdY = gbc.predict(X_test)
-
-                yvalues.append(metrics.accuracy_score(y_test, prdY))
-                # following code prints the data of the graph
-                model_performance(y_test, prdY, estimators[e], max_depth[d], l_rate[l], n_name='  Estim:',
-                                  d_name='depth:', k_name='l_rate:')
-        values[d] = yvalues
-        plt.figure()
-        multiple_line_chart(plt.gca(), estimators, values, 'Gradient Boosting estimators', 'n', 'accuracy',
-                            percentage=False)  # changed to false
-        if save == 'p':
-            plt.savefig('Gradient_Boosting.png')
-        plt.show()
-
-
-GB(dataSample, 'p')
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, stratify=y)
+#
+#     estimators = [5, 50, 100, 150, 200, 300]
+#     l_rate = [0.1]
+#     max_depth = [2, 10, 20]
+#
+#     yvalues = []
+#     for l in range(len(l_rate)):
+#         values = {}
+#         for d in range(len(max_depth)):
+#             yvalues = []
+#             for e in range(len(estimators)):
+#                 gbc = GradientBoostingClassifier(n_estimators=estimators[e], learning_rate=l_rate[l],
+#                                                  max_depth=max_depth[d])
+#                 gbc.fit(X_train, y_train)
+#                 gbc.score(X_test, y_test)
+#                 prdY = gbc.predict(X_test)
+#
+#                 yvalues.append(metrics.accuracy_score(y_test, prdY))
+#                 # following code prints the data of the graph
+#                 model_performance(y_test, prdY, estimators[e], max_depth[d], l_rate[l], n_name='  Estim:',
+#                                   d_name='depth:', k_name='l_rate:')
+#         values[d] = yvalues
+#         plt.figure()
+#         multiple_line_chart(plt.gca(), estimators, values, 'Gradient Boosting estimators', 'n', 'accuracy',
+#                             percentage=False)  # changed to false
+#         if save == 'p':
+#             plt.savefig('Gradient_Boosting.png')
+#         plt.show()
+#
+#
+# GB(dataSample, 'p')
